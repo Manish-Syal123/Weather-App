b602f1370e1d1f223dcb3d8acd59d829837cc4e0
Manish_Syal <manishsyal77@gmail.com>
2022-11-20 00:31:17 +0530

    Added css for temp.js & Started Application

---
+++ src/temp.js
@@ -0,0 +1,12 @@
+// api.openweathermap.org/data/2.5/weather?q=pune&appid=5e27beac707e28e08f4480993dda95df
+import React from 'react'
+
+const temp = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default temp;
